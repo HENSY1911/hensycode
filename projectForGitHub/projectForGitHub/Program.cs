3e9d3ce72a11f732a8b9c1fc55e55ae0a25236ed
HENSY1911 <hensydevirson@gmail.com>
2026-03-23 15:33:59 +0500

    Add files via upload

---
+++ projectForGitHub/projectForGitHub/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Игра 'Угадай число' ===\n");
+
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101); // Число от 1 до 100
+            int attempts = 0;
+            int guess = 0;
+
+            Console.WriteLine("Я загадал число от 1 до 100.");
+            Console.WriteLine("Попробуй угадать!\n");
+
+            while (guess != secretNumber)
+            {
+                Console.Write("Введи свой вариант: ");
+                string input = Console.ReadLine();
+
+                // Проверяем, ввел ли пользователь число
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine("Это не число! Попробуй еще раз.\n");
+                    continue;
+                }
+
+                attempts++;
+
+                if (guess < secretNumber)
+                {
+                    Console.WriteLine("Больше! Попробуй еще.\n");
+                }
+                else if (guess > secretNumber)
+                {
+                    Console.WriteLine("Меньше! Попробуй еще.\n");
+                }
+                else
+                {
+                    Console.WriteLine($"\nПоздравляю! Ты угадал число {secretNumber} за {attempts} попыток!");
+                }
+            }
+
+            Console.WriteLine("\nНажми любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ projectForGitHub/projectForGitHub/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("projectForGitHub")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("projectForGitHub")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("32668f70-d562-4e3d-a5e1-e7bee51717fa")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
